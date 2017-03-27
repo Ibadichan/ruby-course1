fce7dca8a7cdc9d57bc0f475a4eae89ad55816f4
Badicean Ivan <badican01117@gmail.com>
2017-03-28 00:26:31 +0200

    lesson 7

---
+++ lesson-7/route.rb
@@ -0,0 +1,24 @@
+class Route
+  attr_reader :stations
+
+  def initialize(start_station,end_station)
+    @stations = [start_station,end_station]
+    validate!
+  end
+
+  def add_station(station)
+    @stations.insert(-2,station)
+  end
+
+  def remove_station(station)
+    return unless @stations.include?(station)
+    @stations.delete(station) if station != @stations[0] && station != @stations[-1]
+  end
+
+  private
+
+  def validate!
+    raise 'invalid station type!!!' unless @stations.all? {|station| station.is_a?(Station)}
+  end
+end
+
