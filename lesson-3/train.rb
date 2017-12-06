35e78751928f0196707c4c9f7a80710ff8b44379
Ivan Badichan <badican01117@gmail.com>
2017-12-06 23:12:22 +0100

    lesson-3(fix all errors)

---
+++ lesson-3/train.rb
@@ -0,0 +1,55 @@
+class Train
+  attr_reader :type, :number, :cars
+  attr_accessor :speed, :route
+
+  def initialize(number, type, cars)
+    @number = number
+    @type = type
+    @cars = cars
+    @speed = 0
+    @index = 0
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_car
+    @cars += 1 if stopped?
+  end
+
+  def remove_car
+    return if @cars.zero?
+    @cars -= 1 if stopped?
+  end
+
+  def forward
+    @index += 1 if can_next?
+  end
+
+  def backward
+    @index -= 1 if @index > 0
+  end
+
+  def current_station
+    route.stations[@index]
+  end
+
+  def prev_station
+    route.stations[@index - 1] if @index > 0
+  end
+
+  def next_station
+    route.stations[@index + 1] if can_next?
+  end
+
+  private
+
+  def can_next?
+    @index <= route.stations.size
+  end
+
+  def stopped?
+    speed.zero?
+  end
+end
